c330bb4b608b334d26b0c77c90f9526f11a98ddd
nguyenhungsysc <nguyenhungsys@gmail.com>
2017-05-20 22:20:40 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
  error_reporting(0); 
 
-$html = file_get_contents('URL');
+$html = file_get_contents('https://dashboard.heroku.com/apps/nthsysc/deploy/github');
 $dom = new domDocument;
 $dom->loadHTML($html);
 $dom->preserveWhiteSpace = false;
